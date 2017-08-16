f65ead619b16363a97c3e0e5453dd92d60842b79
ddsheard <ddsheard@gmail.com>
2017-08-16 11:38:12 -0400

    last changes

---
+++ src/Nav.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Nav extends Component {
+
+  render () {
+    return (
+      <ul>
+        <li>Home</li>
+        <li>detra</li>
+        <li>Portfolio</li>
+        <li>About</li>
+      </ul>
+    )
+  }
+
+}
+
+export default Nav;
