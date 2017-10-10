f87d309e7b7b09947fff70379f92215a6d93db8c
ddsheard <ddsheard@gmail.com>
2017-10-10 17:40:14 -0400

    create page navigation

---
+++ src/Nav.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.js';
+
+class App extends Component {
+  render () {
+    return (
+      <div>
+        <ul>
+          <li>detra</li>
+          <li>portfolio</li>
+          <li>about</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default Nav;
