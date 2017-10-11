64ba15cfa8a94cd677269f96544254c674d43cc1
ddsheard <ddsheard@gmail.com>
2017-10-10 21:53:03 -0400

    navigation progress

--- src/Nav.js
+++ src/Nav.js
@@ -6,7 +6,7 @@ class App extends Component {
   render () {
     return (
       <div>
-        <ul>
+        <ul className="navigation">
           <li>detra</li>
           <li>portfolio</li>
           <li>about</li>
