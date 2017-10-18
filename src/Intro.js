aa76dee8a9d1b1d8bcc5581dc9432391ded97062
ddsheard <ddsheard@gmail.com>
2017-10-18 12:08:43 -0400

    adding Intro

---
+++ src/Intro.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Intro extends Component {
+  render () {
+    return (
+      <div className='ContainIntro'>
+        <button>What now</button>
+        <h1> @ddsheard Portfolio</h1>
+      </div>
+    );
+  }
+}
+
+export default Intro;
