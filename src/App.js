4dc36661a6215cbd15610f2df5d6b7a534691dfe
ddsheard <ddsheard@gmail.com>
2017-10-18 14:16:39 -0400

    all files merged and up to date

--- src/App.js
+++ src/App.js
@@ -2,10 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Nav from './Nav.js';
-<<<<<<< HEAD
 import Intro from './Intro.js';
-=======
->>>>>>> oct18
 
 class App extends Component {
   render() {
