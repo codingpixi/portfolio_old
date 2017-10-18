5f34689f105baaddcd83a8cce26d7b5ec8c4f496
ddsheard <ddsheard@gmail.com>
2017-10-18 13:15:49 -0400

    fix mess up

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import './Nav.js';
+import Nav from './Nav.js';
 
 class App extends Component {
   render() {
